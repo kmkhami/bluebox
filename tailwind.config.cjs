75b5d829383a809d9b22e0be366f5ffb9c372360
Nicholas Khami <nicholas.khami@gmail.com>
2023-05-19 09:04:18 -0500

    feature: add linting and prettier

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {}
   },
-  plugins: []
+  // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-call
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
 };
